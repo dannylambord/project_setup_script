c3080447170209c31d1daf3b7ca5276bf3eeee6c
Hugh <hughfharding@gmail.com>
2019-07-09 10:55:34 +0000

    working set up script

---
+++ project_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# make sure services are stopped before installing again
+sudo systemctl stop frontend.service
+
+sudo systemctl stop backend.service
+
+
+sudo apt install -y mongodb nodejs npm git
+
+rm -rf TeamAPoolProject*
+
+git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
+
+git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
+
+cd TeamAPoolProjectBackend
+
+git checkout Developer
+
+npm install
+
+cd ..
+
+npm install 
+
+sudo npm -g install @angular/cli
+
+sudo rm -f /etc/systemd/system/frontend.service
+sudo rm -f /etc/systemd/system/backend.service
+
+sudo cp /home/hughfharding/frontend.service /etc/systemd/system/frontend.service
+sudo cp /home/hughfharding/backend.service /etc/systemd/system/backend.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl start mongodb
+
+sudo systemctl start frontend.service
+
+sudo systemctl start backend.service
+
+
