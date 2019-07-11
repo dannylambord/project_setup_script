8d8f4a419887270fab249f8cb3d530e65c949a64
HughH1 <46493493+HughH1@users.noreply.github.com>
2019-07-11 13:49:07 +0100

    Update project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -29,8 +29,8 @@ sudo npm -g install @angular/cli
 sudo rm -f /etc/systemd/system/frontend.service
 sudo rm -f /etc/systemd/system/backend.service
 
-sudo cp ~/terraform/frontend.service /etc/systemd/system/frontend.service
-sudo cp ~/terraform/backend.service /etc/systemd/system/backend.service
+sudo cp ~/frontend.service /etc/systemd/system/frontend.service
+sudo cp ~/backend.service /etc/systemd/system/backend.service
 
 sudo systemctl daemon-reload
 
