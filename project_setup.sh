85ef2664fac642037c83bddbaf75c6dbed215745
HughH1 <46493493+HughH1@users.noreply.github.com>
2019-07-11 14:02:59 +0100

    Update project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -29,8 +29,8 @@ sudo npm -g install @angular/cli
 sudo rm -f /etc/systemd/system/frontend.service
 sudo rm -f /etc/systemd/system/backend.service
 
-sudo cp ~/frontend.service /etc/systemd/system/frontend.service
-sudo cp ~/backend.service /etc/systemd/system/backend.service
+sudo cp ~/project_setup_script/frontend.service /etc/systemd/system/frontend.service
+sudo cp ~/project_setup_script/backend.service /etc/systemd/system/backend.service
 
 sudo systemctl daemon-reload
 
