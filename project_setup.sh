0828c34b47243457efa9af16026cda5de57ab79d
Hugh Harding <hughfharding@gmail.com>
2019-07-12 11:50:41 +0100

    first commit

--- project_setup.sh
+++ project_setup.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
 # make sure services are stopped before installing again
-sudo systemctl stop frontend.service
-
-sudo apt-get install -y nodejs
-
-rm -rf TeamAPoolProject*
-
-git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
-
-cd TeamAPoolProjectUI
-
-yes | npm install 
-
-yes | sudo npm -g install @angular/cli
-
-cd ..
-
-sudo rm -f /etc/systemd/system/frontend.service
-
-sudo cp ./project_setup_script/frontend.service /etc/systemd/system/frontend.service
+sudo apt install -y mongodb npm git
 
 sudo systemctl daemon-reload
 
-sudo systemctl start frontend.service
+sudo systemctl start mongodb
